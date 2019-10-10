1d8f4f3eedb856d2c01c9c74dffe97dd1c245b93
Reese Anschultz <reese@anschultz.org>
2019-10-10 12:16:22 -0700

    Successfully uses the example from XamlReader.Load(Stream) to serialize and unserialize and object

--- TestCases/Program.cs
+++ TestCases/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Xml;
 
 namespace TestCases
 {
     class Program
     {
+        [STAThread] // Prevent runtime message: "The calling thread must be STA, because many UI components require this."
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Button originalButton = new Button();
+            originalButton.Height = 50;
+            originalButton.Width = 100;
+            originalButton.Background = Brushes.AliceBlue;
+            originalButton.Content = "Click Me";
+
+            // Save the Button to a string.
+            string savedButton = XamlWriter.Save(originalButton);
+
+            // Load the button
+            StringReader stringReader = new StringReader(savedButton);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            Button readerLoadButton = (Button)XamlReader.Load(xmlReader);
         }
     }
 }
